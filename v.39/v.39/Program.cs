24076c40724b985c0d76f96b9b29ddadd728a9f1
Bledon <bledonqyqalla@gmail.com>
2023-09-27 14:24:01 +0200

    add ignore

---
+++ v.39/v.39/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+Console.WriteLine("Book Your Ticket Here");
+
+// Fråga ålder?
+int age = v._39.utility.GetAge();
+// Fråga plats?
+string place = v._39.utility.GetSeat();
+
+// Ge pris.
+decimal cost = v._39.utility.GetCost();
+// Visa skatt.
+string tax = v._39.utility.GetTax();
+// Visa Biljett.
+int ticket = v._39.utility.TicketNumber();
+
+// Ålder
+Console.WriteLine("Please Enter Your age");
+
+// Plats 
+Console.WriteLine("Seated och Standing ticket?");
+
+
+//Svar. 
+Console.WriteLine($"Ticket Prise is{cost}kr");
+Console.WriteLine($"Taxammount: {tax}kr");
+Console.WriteLine($"Your Ticket number is:{ticket}");
+
+
+
+Console.ReadLine();
+
+
+
+
+
+
---
+++ v.39/v.39/utility.cs
@@ -0,0 +1,98 @@
+﻿using System;
+namespace v._39
+{
+    public class utility
+    {
+
+        //Metod ålder.
+        public static int GetAge()
+        {
+            Console.WriteLine("Please Enter Your age");
+            int age = Console.ReadLine();
+
+            return age;
+        }
+
+        //metod plats.
+        public static string GetSeat()
+        {
+            string place = Console.ReadLine();
+            if (place != "Seated" && place != "Standig")
+            {
+
+                Console.WriteLine("Please Enter Seated or Standing");
+            }
+
+            return place;
+        }
+
+        // Metod summa. 
+        public static decimal GetCost()
+        {
+            string cost = Console.ReadLine();
+            if (place == "Seated")
+            {
+
+                if (age > 11)
+                {
+                    cost = 50;
+                }
+
+                else if (11 <=age && <65)
+                {
+                    cost = 170;
+                }
+
+                else (64 < age);
+                {
+                    cost = 100;
+                }
+
+            }
+            if (place == "Standing") ;
+            {
+
+                if (age > 11)
+                {
+                    cost = 25;
+                }
+
+                else if (11 <= age && <65)
+                {
+                    cost = 110;
+                }
+
+                else (64 < age);
+                {
+                    cost = 60;
+                }
+
+            }
+
+
+
+            return cost;
+        }
+
+        // metod skatt 
+        public static string GetTax()
+        {
+
+            string tax = (1 - (1 / 1.06)) * cost;
+
+            return tax;
+        }
+
+        public static int TicketNumber()
+        {
+            Random Random = new();
+            int ticket = Random.Next(1, 8000);
+
+            return ticket;
+
+        }
+    }
+}
+
+
+
